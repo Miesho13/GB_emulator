750b097fc763036cc9d7c67809b19eddb0ccfb07
Marcin <marcin.ryzewskii@gmail.com>
2024-09-24 22:31:25 +0200

    Define context structure for GB.

--- src/GB.h
+++ src/GB.h
@@ -1,5 +1,15 @@
 #ifndef __GB_H__
 #define __GB_H__
 
+#include "mem.h"
+#include "cpu.h"
+
+typedef struct { 
+    cpu_t cpu;
+    mem_t mem_ctx;
+} gb_ctx_t;
+
+void init(gb_ctx_t gb_ctx);
+void run(gb_ctx_t gb_ctx);
 
 #endif
