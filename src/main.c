10abb437c8e3339893d3564839e5590f439802f5
marcin ryzewski <marcin.ryzewskii@gmail.com>
2025-05-04 23:07:59 +0200

    Implementation the monitoring function

--- src/main.c
+++ src/main.c
@@ -4,15 +4,24 @@
 #include "GB_int.h"
 #include "GB.h"
 
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    
-    printf("[INFO] Start emulation.\n");
-//  load_program(...);
-    while(1) {
-//      step(...);
-        break;
+void gb_monitor(gb_ctx_t *gb) {
+    for (int i = 0; i < CARTRIGBE_SIZE; i++) {
+        printf("0x%2x ", gb->mem.buffer[i]);
+        if (i%10 == 0 && i != 0) {
+            printf("\n");
+        }
     }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        puts("Error: invalid arguments.\n\tUsage: GB <path_to_rome>");
+        return 1;
+    }
+    gb_ctx_t gb;
+
+    load_cartrigbe(&gb, argv[1]);
+    gb_monitor(&gb);
+
     return 0;
 }
