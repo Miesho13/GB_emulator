420831d5798aab7d0bfd0ea0eaca6091b52768bf
Miesho <marcin.ryzewskii@gmail.com>
2025-08-04 00:13:13 +0200

    cpu: add: ld opcode operation

--- src/cpu.c
+++ src/cpu.c
@@ -52,7 +52,197 @@ void cpu_init() {
 void cpu_step(cpu_t *cpu, mem_t *mem) {
 
     switch(memory(REG_PC)) {
+    /* ---- 8 bit loads  ---- */
+        case 0x06: {
+            LD(
+                REG_B,
+                memory(REG_PC + 1);
+            );
+            REG_PC += 2;
+            CYCLE_DLY(4);
+            break;
+        }
 
+        case 0x0e: {
+            LD(
+                REG_C,
+                memory(REG_PC + 1);
+            );
+            REG_PC += 2;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x16: {
+            LD(
+                REG_D,
+                memory(REG_PC + 1);
+            );
+            REG_PC += 2;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x1e: {
+            LD(
+                REG_E,
+                memory(REG_PC + 1);
+            );
+            REG_PC += 2;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x26: {
+            LD(
+                REG_H,
+                memory(REG_PC + 1);
+            );
+            REG_PC += 2;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x2e: {
+            LD(
+                REG_L,
+                memory(REG_PC + 1);
+            );
+            REG_PC += 2;
+            CYCLE_DLY(4);
+            break;
+        }
+
+    /* --------- LD r2 into r1 load --------- */
+        case 0x7f: {
+            LD(
+                REG_A,
+                REG_A
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x78: {
+            LD(
+                REG_A,
+                REG_B
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x79: {
+            LD(
+                REG_A,
+                REG_C
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x7a: {
+            LD(
+                REG_A,
+                REG_D
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x7b: {
+            LD(
+                REG_A,
+                REG_E
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x7c: {
+            LD(
+                REG_A,
+                REG_H
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x7d: {
+            LD(
+                REG_A,
+                REG_L
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x7e: {
+            LD(
+                REG_A,
+                REG_HL
+            );
+            REG_PC += 1;
+            CYCLE_DLY(8);
+            break;
+        }
+
+        case 0x40: {
+            LD(
+                REG_B,
+                REG_B
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x41: {
+            LD(
+                REG_B,
+                REG_C
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x42: {
+            LD(
+                REG_B,
+                REG_D
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x43: {
+            LD(
+                REG_B,
+                REG_E
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
+
+        case 0x44: {
+            LD(
+                REG_B,
+                REG_E
+            );
+            REG_PC += 1;
+            CYCLE_DLY(4);
+            break;
+        }
     }
 }
 
