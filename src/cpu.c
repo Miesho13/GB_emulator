7a91e24d0f6d8b3c0f91085d16f9d069076744b4
Miesho <marcin.ryzewskii@gmail.com>
2025-08-03 23:30:40 +0200

    src/cpu.c: add: symple define for basic cpu opertaion.

--- src/cpu.c
+++ src/cpu.c
@@ -1,75 +1,52 @@
 #include "cpu.h"
 
-void ld_r1r2(uint8_t* r1, uint8_t *r2) {
-    *r1 = *r2;
+static cpu_t cpu_;
+static mem_t mem_;
+
+#define REG_A cpu_.af.u8[1]
+#define REG_B cpu_.bc.u8[1]  
+#define REG_C cpu_.bc.u8[0] 
+#define REG_D cpu_.de.u8[1] 
+#define REG_E cpu_.de.u8[0] 
+#define REG_F cpu_.af.u8[0] 
+#define REG_H cpu_.hl.u8[1]
+#define REG_L cpu_.hl.u8[0]
+
+#define REG_AF cpu_.af.u16
+#define REG_BC cpu_.bc.u16 
+#define REG_DE cpu_.de.u16 
+#define REG_HL cpu_.hl.u16
+
+#define REG_SP cpu_.sp
+#define REG_PC cpu_.pc
+
+#define memory(addr) mem_.buffer[addr]
+
+#define LD(L, R)  \
+    do {          \
+        L = R;    \
+    } while(0)    \
+
+cpu_t* cpu_inst() {
+    return &cpu_;
+}
+
+mem_t* mem_inst() {
+    return &mem_;
+}
+
+void cpu_init() {
+
 }
 
 void cpu_step(cpu_t *cpu, mem_t *mem) {
 
-    switch(mem->buffer[cpu->pc]) {
-        case 0x06:
-            ld_r1r2(&cpu->bc.u8[1], &mem->buffer[cpu->pc + 1]);
-            cpu->pc += 2;
-            break;
-
-        case 0x0e:
-            ld_r1r2(&cpu->bc.u8[0], &mem->buffer[++cpu->pc + 1]);
-            cpu->pc += 2;
-            break;
-
-        case 0x16:
-            ld_r1r2(&cpu->de.u8[1], &mem->buffer[cpu->pc + 1]);
-            cpu->pc += 2;
-            break;
-
-        case 0x1e:
-            ld_r1r2(&cpu->de.u8[0], &mem->buffer[cpu->pc + 1]);
-            cpu->pc += 2;
-            break;
-
-        case 0x26:
-            ld_r1r2(&cpu->hl.u8[1], &mem->buffer[cpu->pc + 1]);
-            cpu->pc += 2;
-            break;
-
-        case 0x2e:
-            ld_r1r2(&cpu->hl.u8[0], &mem->buffer[cpu->pc + 1]);
-            cpu->pc += 2;
-            break;
-            
-        case 0x7f:
-            ld_r1r2(&cpu->af.u8[1], &cpu->af.u8[1]);
-            break;
-
-        case 0x78:
-            ld_r1r2(&cpu->af.u8[1], &cpu->bc.u8[1]);
-            break;
-
-        case 0x79:
-            ld_r1r2(&cpu->af.u8[1], &cpu->bc.u8[0]);
-            break;
-
-        case 0x7a:
-            ld_r1r2(&cpu->af.u8[1], &cpu->de.u8[1]);
-            break;
-
-        case 0x7b:
-            ld_r1r2(&cpu->af.u8[1], &cpu->de.u8[0]);
-            break;
-
-        case 0x7c:
-            ld_r1r2(&cpu->af.u8[1], &cpu->hl.u8[1]);
-            break;
-
-        case 0x7d:
-            ld_r1r2(&cpu->af.u8[1], &cpu->hl.u8[0]);
-            break;
-
-        case 0x7d:
-            ld_r1r2(&cpu->af.u8[1], &cpu->hl.u8[0]);
-            break;
+    switch(memory(REG_PC)) {
 
     }
 }
+
+
+
 
 // 0x06 0x22
